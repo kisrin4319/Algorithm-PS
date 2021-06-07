90374bb7da90ec727bf16b950fc81789b7d56c67
kisrin4319 <kisrin4319@Naver.com>
2021-06-07 09:15:28 +0900

    Update LV2 - 조이스틱.java

--- Programmers/LV2 - 조이스틱.java
+++ Programmers/LV2 - 조이스틱.java
@@ -1,3 +1,5 @@
+//다른 분들의 소스 참조해서 해결
+// cnt = Math.min(cnt,idx+len-next+Math.min(idx,len-next)); 부분이 핵심이다.
 import java.util.*;
 class Solution {
     public int solution(String name) {
@@ -5,13 +7,14 @@ class Solution {
         int answer = 0;        
         char[] ch = name.toCharArray();		
         int len = name.length();
-		int cnt = len-1;
+	int cnt = len-1;  //정방향으로 쭉 변경하면서 가는 경우
         
         for(int idx = 0; idx<ch.length;idx++)
         {
             answer+= Math.min((int)(ch[idx]-'A'),(int)('Z'-ch[idx] +1));
             
-            int next = idx+1;
+	    //1. 정방향 , 2. 역방향 , 3. 정방향 후 다시 역방향
+            int next = idx+1; // 다음 위치
             while(next<name.length() && name.charAt(next) == 'A'){
                 next++;
             }
